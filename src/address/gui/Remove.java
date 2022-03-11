1e3f8640148e34060c8805bd6ec3fb65de9a000a
Andrea Labra Orozco <andrealabra21@gmail.com>
2022-03-10 16:23:32 -0800

    display and new

--- src/address/gui/Remove.java
+++ src/address/gui/Remove.java
@@ -12,5 +12,6 @@ public class Remove {
     private JButton addButton;
     private JTextField removeAContactFromTextField;
     private JButton cancelButton;
+    private JEditorPane editorPane1;
 }
 
