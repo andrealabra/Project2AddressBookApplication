6a62d15846e7a6bced3d78e723c3f649fa0bd239
Adrian Anderson <74568128+andersonadrian@users.noreply.github.com>
2022-03-11 15:23:14 -0800

    adding the JtextBox and JLabels to the new form

--- src/address/gui/New.java
+++ src/address/gui/New.java
@@ -1,4 +1,16 @@
 package address.gui;
 
+import javax.swing.*;
+
 public class New {
+    private JLabel firstNameLabel;
+    private JTextField firstNameTextField;
+    private JLabel lastNameLabel;
+    private JTextField lastNameTextField;
+    private JLabel streetLabel;
+    private JTextField streetTextField;
+    private JLabel cityLabel;
+    private JTextField cityTextField;
+    private JLabel stateLabel;
+    private JTextField stateTextField;
 }
--- src/address/gui/Remove.java
+++ src/address/gui/Remove.java
@@ -1,15 +1,10 @@
 package address.gui;
 
-import address.data.AddressBook;
-import address.data.AddressEntry;
-
 import javax.swing.*;
-import java.awt.event.*;
-import java.util.List;
 
 public class Remove {
     private JPanel panel1;
-    private JButton addButton;
+    private JButton deleteButton;
     private JTextField removeAContactFromTextField;
     private JButton cancelButton;
     private JEditorPane editorPane1;
