9cfb9c90f3e152a13de22c7480efd24f7d296ee8
Adrian Anderson <74568128+andersonadrian@users.noreply.github.com>
2022-03-11 17:22:45 -0800

    Added the event listener to the New Form

--- src/address/gui/New.java
+++ src/address/gui/New.java
@@ -1,22 +1,39 @@
 package address.gui;
 
+import address.data.AddressEntry;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class New {
     private JLabel firstNameLabel;
     private JTextField firstNameTextField;
     private JLabel lastNameLabel;
-    private JTextField lastNameTextField;
-    private JLabel streetLabel;
-    private JTextField streetTextField;
     private JLabel cityLabel;
     private JTextField cityTextField;
-    private JLabel stateLabel;
     private JTextField stateTextField;
-    private JLabel zipCodeLabel;
     private JTextField zipCodeTextField;
+    private JTextField lastNameTextField;
+    private JLabel stateLabel;
+    private JLabel zipCodeLabel;
     private JLabel phoneNumberLabel;
     private JTextField phoneNumberTextField;
     private JLabel emailLabel;
     private JTextField emailTextField;
+    private JLabel streetLabel;
+    private JTextField streetTextField;
+    private JButton submitButton;
+
+    AddressEntry newEntry;
+
+    public New(){
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                newEntry = new AddressEntry(firstNameTextField.getText(),lastNameTextField.getText(),streetTextField.getText(),cityTextField.getText(),stateTextField.getText(),
+                        Integer.parseInt(zipCodeTextField.getText()),emailTextField.getText(),phoneNumberTextField.getText());
+            }
+        });
+    }
 }
